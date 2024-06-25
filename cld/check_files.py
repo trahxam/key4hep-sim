030a0cd801b512ab67d7aeec65c1c8926405fc04
Joosep Pata <joosep.pata@cern.ch>
2024-06-25 11:36:40 +0300

    update CLD

--- cld/check_files.py
+++ cld/check_files.py
@@ -5,7 +5,7 @@ outpath = "/local/joosep/cld_edm4hep/2024_05/"
 
 #pythia card, start seed, end seed
 samples = [
-    ("p8_ee_tt_ecm365",         100001, 100011),
+    ("p8_ee_tt_ecm365",         100001, 100111),
 ]
 
 if __name__ == "__main__":
--- cld/run_sim.sh
+++ cld/run_sim.sh
@@ -9,15 +9,15 @@ set -x
 env
 df -h
 
-NEV=100
-NUM=$1 #random seed
-SAMPLE=$2 #main card
+export NEV=100
+export NUM=$1 #random seed
+export SAMPLE=$2 #main card
 
 #Change these as needed
-OUTDIR=/local/joosep/cld_edm4hep/2024_05/
-SIMDIR=/home/joosep/key4hep-sim/cld/CLDConfig/CLDConfig
-WORKDIR=/scratch/local/$USER/${SAMPLE}_${SLURM_JOB_ID}
-FULLOUTDIR=${OUTDIR}/${SAMPLE}
+export OUTDIR=/local/joosep/cld_edm4hep/2024_05/
+export SIMDIR=/home/joosep/key4hep-sim/cld/CLDConfig/CLDConfig
+export WORKDIR=/scratch/local/$USER/${SAMPLE}_${SLURM_JOB_ID}
+export FULLOUTDIR=${OUTDIR}/${SAMPLE}
 
 mkdir -p $FULLOUTDIR
 
@@ -33,11 +33,19 @@ cp -R $SIMDIR/CLDReconstruction.py ./
 echo "Random:seed=${NUM}" >> card.cmd
 cat card.cmd
 
+echo "
+#!/bin/bash
+set -e
 source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
-
+env
 k4run pythia.py -n $NEV --Dumper.Filename out.hepmc --Pythia8.PythiaInterface.pythiacard card.cmd
-ddsim -I out.hepmc -N -1 -O out_SIM.root --compactFile $K4GEO/FCCee/CLD/compact/CLD_o2_v05/CLD_o2_v05.xml --steeringFile cld_steer.py
+ddsim -I out.hepmc -N -1 -O out_SIM.root --compactFile \$K4GEO/FCCee/CLD/compact/CLD_o2_v05/CLD_o2_v05.xml --steeringFile cld_steer.py
 k4run CLDReconstruction.py --inputFiles out_SIM.root --outputBasename out_RECO --num-events -1
+" > sim.sh
+
+cat sim.sh
+
+singularity exec -B /cvmfs -B /scratch -B /local docker://ghcr.io/key4hep/key4hep-images/alma9:latest bash sim.sh
 
 #Copy the outputs
 cp out_RECO_edm4hep.root $FULLOUTDIR/reco_${SAMPLE}_${NUM}.root
