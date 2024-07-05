6ba58594dfe07d1c5f75964dbd8e00348ab871c2
Joosep Pata <joosep.pata@cern.ch>
2024-07-05 14:47:33 +0300

    create dirs

--- cld/check_files.py
+++ cld/check_files.py
@@ -1,16 +1,18 @@
 import os
 
 #check for file presence in this path
-outpath = "/local/joosep/cld_edm4hep/2024_05/"
+outpath = "/local/joosep/cld_edm4hep/2024_05_full/"
 
 #pythia card, start seed, end seed
 samples = [
-    ("p8_ee_tt_ecm365",         100001, 100111),
+    ("p8_ee_tt_ecm365",         100001, 101010),
 ]
 
 if __name__ == "__main__":
     for sname, seed0, seed1 in samples:
+        os.makedirs(f"{outpath}/{sname}/root", exist_ok=True)
+        os.makedirs(f"{outpath}/{sname}/sim", exist_ok=True)
         for seed in range(seed0, seed1):
             #check if output file exists, and print out batch submission if it doesn't
-            if not os.path.isfile("{}/{}/reco_{}_{}.root".format(outpath, sname, sname, seed)):
+            if not os.path.isfile(f"{outpath}/{sname}/root/reco_{sname}_{seed}.root"):
                 print("sbatch run_sim.sh {} {}".format(seed, sname)) 
--- cld/run_sim.sh
+++ cld/run_sim.sh
@@ -14,7 +14,7 @@ export NUM=$1 #random seed
 export SAMPLE=$2 #main card
 
 #Change these as needed
-export OUTDIR=/local/joosep/cld_edm4hep/2024_05/
+export OUTDIR=/local/joosep/cld_edm4hep/2024_05_full/
 export SIMDIR=/home/joosep/key4hep-sim/cld/CLDConfig/CLDConfig
 export WORKDIR=/scratch/local/$USER/${SAMPLE}_${SLURM_JOB_ID}
 export FULLOUTDIR=${OUTDIR}/${SAMPLE}
@@ -45,10 +45,12 @@ k4run CLDReconstruction.py --inputFiles out_SIM.root --outputBasename out_RECO -
 
 cat sim.sh
 
-singularity exec -B /cvmfs -B /scratch -B /local docker://ghcr.io/key4hep/key4hep-images/alma9:latest bash sim.sh
+# singularity exec -B /cvmfs -B /scratch -B /local docker://ghcr.io/key4hep/key4hep-images/alma9:latest bash sim.sh
+singularity exec -B /cvmfs -B /scratch -B /local /home/software/singularity/alma9.simg bash sim.sh
 
 #Copy the outputs
-cp out_RECO_edm4hep.root $FULLOUTDIR/reco_${SAMPLE}_${NUM}.root
-cp out.hepmc $FULLOUTDIR/sim_${SAMPLE}_${NUM}.hepmc
+cp out_RECO_edm4hep.root $FULLOUTDIR/root/reco_${SAMPLE}_${NUM}.root
+bzip2 out.hepmc
+cp out.hepmc.bz2 $FULLOUTDIR/sim/sim_${SAMPLE}_${NUM}.hepmc.bz2
 
 rm -Rf $WORKDIR
